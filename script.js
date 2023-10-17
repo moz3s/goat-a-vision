3494a88e4652d1604e6023bd35fac1a74aa9ae0a
moz3s <147497955+moz3s@users.noreply.github.com>
2023-10-18 00:21:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function toggleDropdown() {
-    var dropdownMenu = document.getElementById('dropdown-menu');
-    dropdownMenu.classList.toggle('open');
+menu = document.getElementById("menu");
+
+const openMenu = () => {
+    menu.style.transform = "translate(0%)";
+}
+
+const closeMenu = () => {
+    menu.style.transform = "translateX(-100%)";
 }
