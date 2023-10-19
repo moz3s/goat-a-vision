5b4a16d13372ffe0ad2f83a01d3134a33f1a82a1
moz3s <147497955+moz3s@users.noreply.github.com>
2023-10-19 14:48:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const menu = document.getElementById("menu");
-const op = document.getElementById('open')
-const openMenu = () => {
-    menu.style.transform = "translate(0%)";
-    op.style.display = 'none';
-}
-
-const closeMenu = () => {
+const op = document.getElementById('open');
+const cl = document.getElementById('close');
+let clickCount = 0;
+const body = document.querySelector("body");
+function menuOpening(){
+   if (clickCount%2 === 0){
     menu.style.transform = "translateX(-100%)";
-    op.style.display = 'block';
+    cl.style.transform = "translateX(30%)";
+   }
+   else{
+    menu.style.transform = "translate(0%)";
+    cl.style.transform = "translateX(0%)";
+   }
+   clickCount++;
 }
+cl.addEventListener("click", menuOpening);
