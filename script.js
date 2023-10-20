5548370f8da8e5ecdfed793a82447a77e38eec40
moz3s <147497955+moz3s@users.noreply.github.com>
2023-10-20 16:29:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const menu = document.getElementById("menu");
 const cl = document.getElementById('close');
 let clickCount = 0;
-function menuOpening(){
-   if (clickCount%2 === 0){
-    menu.classList.remove('menu1');
-    menu.classList.add('menu2');
-    cl.classList.remove('close1');
-    cl.classList.add('close2');
-   }
-   else{
-    menu.classList.remove('menu2');
-    menu.classList.add('menu1');
-    cl.classList.add('close1');
-    cl.classList.remove('close2');
-   }
-   clickCount++;
+function handleScreenSize() {
+    if (window.innerWidth > 500) {
+        function menuOpening() {
+            if (clickCount % 2 === 0) {
+                // első kattintás
+                menu.style.transform = "translateX(-100%)";
+                cl.style.transform = "translateX(360%)";
+            } else {
+                // második kattintás
+                menu.style.transform = "translateX(0%)";
+                cl.style.transform = "translateX(0%)";
+            }
+            clickCount++;
+        }
+        cl.addEventListener("click", menuOpening);
+    } else {
+        function menuOpening() {
+            if (clickCount % 2 === 0) {
+                // első kattintás
+                menu.style.transform = "translateX(0%)";
+                cl.style.transform = "translateX(0%)";
+            } else {
+                // második kattintás
+                menu.style.transform = "translateX(-100%)";
+                cl.style.transform = "translateX(360%)";
+            }
+            clickCount++;
+        }
+        cl.addEventListener("click", menuOpening);
+    }
 }
-cl.addEventListener("click", menuOpening);
+
+// Az eseménykezelő hozzárendelése a képernyő méretének változásához
+window.addEventListener('resize', handleScreenSize);
+
+// Az eseménykezelő kezdeti meghívása az oldal betöltésekor
+handleScreenSize();
