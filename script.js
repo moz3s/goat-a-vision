51923c5f0d68efd85408e5d5db119a4af55a7661
moz3s <147497955+moz3s@users.noreply.github.com>
2023-10-20 21:06:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
 const menu = document.getElementById("menu");
 const cl = document.getElementById('close');
 let clickCount = 0;
-function handleScreenSize() {
-    if (window.innerWidth > 500) {
-        function menuOpening() {
-            if (clickCount % 2 === 0) {
-                // első kattintás
-                menu.style.transform = "translateX(-100%)";
-                cl.style.transform = "translateX(360%)";
-            } else {
-                // második kattintás
-                menu.style.transform = "translateX(0%)";
-                cl.style.transform = "translateX(0%)";
-            }
-            clickCount++;
+ function menuOpening(){
+    if (window.innerWidth > 500){
+        if (clickCount % 2 === 0) {
+            // első kattintás
+            menu.style.transform = "translateX(-100%)";
+            cl.style.transform = "translateX(360%)";
+        } else {
+            // második kattintás
+            menu.style.transform = "translateX(0%)";
+            cl.style.transform = "translateX(0%)";
         }
-        cl.addEventListener("click", menuOpening);
-    } else {
-        function menuOpening() {
-            if (clickCount % 2 === 0) {
-                // első kattintás
-                menu.style.transform = "translateX(0%)";
-                cl.style.transform = "translateX(0%)";
-            } else {
-                // második kattintás
-                menu.style.transform = "translateX(-100%)";
-                cl.style.transform = "translateX(360%)";
-            }
-            clickCount++;
-        }
-        cl.addEventListener("click", menuOpening);
+        clickCount++;
     }
-}
-
-// Az eseménykezelő hozzárendelése a képernyő méretének változásához
-window.addEventListener('resize', handleScreenSize);
-
-// Az eseménykezelő kezdeti meghívása az oldal betöltésekor
-handleScreenSize();
+    else{
+        if (clickCount % 2 === 0) {
+            // első kattintás
+            menu.style.transform = "translateX(0%)";
+            cl.style.transform = "translateX(0%)";
+        } else {
+            // második kattintás
+            menu.style.transform = "translateX(-100%)";
+            cl.style.transform = "translateX(360%)";
+        }
+        clickCount++;
+    }
+ }
+ cl.addEventListener("click", menuOpening);
